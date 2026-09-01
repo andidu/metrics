69d7673531081cad912b16aa8fbcfa2c67621cc7
andreidugin <andrei@alohabrowser.com>
2026-09-01 18:36:41 +0300

    fix gauge format

--- internal/service/service.go
+++ internal/service/service.go
@@ -44,7 +44,7 @@ func (m memStorageImpl) UpdateCounter(name string, value int) {
 func (m memStorageImpl) GetGauge(name string) (string, bool) {
 	fval, ok := m.gauges[name]
 	if ok {
-		return strconv.FormatFloat(fval, 'f', 3, 64), true
+		return strconv.FormatFloat(fval, 'f', 2, 64), true
 	}
 	return "", false
 }
